c426e519e9c18fb5d9c9dfd7bdefc61f6f068733
Vitaly Gromov <gromovitaly@gmail.com>
2024-05-22 06:09:04 +0300

    12

--- Swift_Coding_Challenges_Hudson_2018/Chapter1.Strings/Challenge12.swift
+++ Swift_Coding_Challenges_Hudson_2018/Chapter1.Strings/Challenge12.swift
@@ -7,3 +7,35 @@
 
 
 import Foundation
+
+func testChallenge12() {
+    let args = ["swift switch swill swim", "flip flap flop"]
+    let args2 = [("swift switch swill swim","swi"), ("flip flap flop", "fl")]
+    
+    for i in args2 {
+        func1(arg: i.0) == i.1 ? print("success") : print("fail")
+    }
+}
+
+func func1(arg: String) -> String {
+    var result = ""
+    var offset = 1
+    var gotIt = false
+    var words = arg.split(separator: " ", omittingEmptySubsequences: true)
+    for i in 1...words[0].count {
+        for j in words {
+            if j.contains(String(words[0].prefix(offset))) {
+                gotIt = false
+            } else {
+                gotIt = true
+            }
+        }
+        if gotIt {
+            result = String(words[0].prefix(offset - 1))
+        } else {
+            offset += 1
+        }
+    }
+    print("result is", result)
+    return result
+}
